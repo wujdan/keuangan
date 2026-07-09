b0441a0707b3c466b49e4b2b1b37c7fc4e4bc752
wujdan <danuttholibin19@gmail.com>
2026-07-09 20:36:45 +0700

    perbaiki api apps3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Ganti nilai di bawah dengan URL Web App hasil deploy Google Apps Script kamu.
 // Lihat README.md bagian "Setup Google Sheets & Apps Script" untuk cara mendapatkannya.
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxSkOnGyhiYPZLyhbXkDF7msstAfSCjTWtEDWOncRU43uFiJLJuaD85pADB-Cy7QEFU/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwjVhxc-FVNqaEZPSn81p0HdaIpjywBpCAeRjOb0EGaBrbcIsXefiWmmWiqhPAjI7s/exec"
 };
